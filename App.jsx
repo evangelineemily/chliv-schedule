14ac2b0f8ed15a32a6f63f50d1bb5b2264c645e1
evangelineemily <evangelineemily0114@gmail.com>
2026-06-15 14:48:27 +0800

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,5 +1,5 @@
 import { useState, useMemo, useEffect } from "react";
-import { useFirestore } from "./useFirestore";
+import { useFirestore } from "/useFirestore";
 
 // ── ACCOUNTS ─────────────────────────────────────────────
 const ACCOUNTS = {
